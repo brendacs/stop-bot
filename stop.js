cf28c6499821a5f23e508b9952876ef49000e96e
Brenda Zhang <brenda.zhang2@gmail.com>
2017-07-28 21:15:38 -0700

    Add go command, but not quite functioning yet

--- stop.js
+++ stop.js
@@ -25,11 +25,35 @@ bot.on('ready', (evt) => {
 
 bot.on('message', (msg) => {
   string = msg.content
-  if (string.toLowerCase().indexOf('kek') !== -1 || string.toLowerCase().indexOf('kèk') !== -1) {
-    const date = new Date().toString();
+  const date = new Date().toString();
+  let admin = msg.member.hasPermission('ADMINISTRATOR')
+
+  if (msg.toString().substring(0, 1) === '!') {
+    const args = msg.toString().substring(1).split(' ');
+    const cmd = args[0];
+    const subcmd = args[1];
+    if (cmd === 'stop') {
+      msg.channel.send(date);
+      msg.channel.send('IT\'S TIME TO STOP.');
+      if (subcmd === 'video') {
+        msg.channel.send('https://www.youtube.com/watch?v=2k0SmqbBIpQ');
+      } else {
+        return;
+      }
+    } else if (cmd === 'go') {
+      if (!admin) {
+        msg.reply('you can\'t use this command.');
+      } else if (admin && typeof subcmd !== 'undefined') {
+        msg.channel.send('`' + subcmd + '`' + ' will no longer be stopped.');
+      } else {
+        msg.reply('what do you want to unstop?');
+      }
+    } else {
+      return;
+    }
+  } else if (string.toLowerCase().indexOf('kek') !== -1 || string.toLowerCase().indexOf('kèk') !== -1) {
     msg.channel.send(date);
     msg.reply('IT\'S TIME TO STOP.');
-    msg.channel.send('https://www.youtube.com/watch?v=2k0SmqbBIpQ');
   } else {
     return;
   }
