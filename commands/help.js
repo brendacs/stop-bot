19dc67efca3ad9aad9f735922c02c4ec4c86e348
Brenda Zhang <brenda.zhang2@gmail.com>
2017-08-02 13:35:03 -0700

    Use template string for help module clarity

--- commands/help.js
+++ commands/help.js
@@ -2,9 +2,14 @@ exports.run = (msg, cmd, richEmbed) => {
   const help = require('./help.json');
   const embed = richEmbed
     .setColor('#ff0000')
-    .setDescription(help.headings.general + help.general + '\n' + help.headings.stop + help.stop + '\n' + help.headings.delete + help.delete + '\n' + help.headings.go + help.go)
-    .addBlankField(true)
-    .addField('Anything else', 'For more information, visit the [Stop Bot GitHub](https://github.com/brendacs/stop-bot)')
+    .setDescription(
+
+`${help.headings.general} ${help.general}
+${help.headings.stop} ${help.stop}
+${help.headings.delete} ${help.delete}
+${help.headings.go} ${help.go}
+For more information, visit the [Stop Bot GitHub](https://github.com/brendacs/stop-bot)`)
+    
   if (cmd === 'help') {
     msg.channel.send({embed});
   } else {
