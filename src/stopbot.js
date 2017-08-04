7a4d4385a5bf8f5c2bf76b910689356020aba725
Brenda Zhang <brenda.zhang2@gmail.com>
2017-08-04 12:39:10 -0700

    Add server count for discord pw bot list

--- src/stopbot.js
+++ src/stopbot.js
@@ -13,6 +13,7 @@ logger.level = 'debug';
 
 const TOKEN = auth.token;
 const DBOT_TOKEN = auth.dbot_token;
+const PWBOT_TOKEN = auth.pwbot_token;
 
 const bot = new Discord.Client({
   token: TOKEN,
@@ -24,13 +25,21 @@ bot.on('ready', (evt) => {
   logger.info('Logged in as: ');
   logger.info(bot.user.username + ' - ' + bot.user.id);
 
-  // Server count for discord bot list
+  // Server count for discord bot lists
   request.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
     .set('Authorization', DBOT_TOKEN)
     .send({server_count: bot.guilds.size})
     .end(err => {
       if (err) return console.error(err);
-      console.log("Success!");
+      console.log("Success (dbot)!");
+    });
+
+  request.post(`https://bots.discord.pw/api/bots/${bot.user.id}/stats`)
+    .set('Authorization', PWBOT_TOKEN)
+    .send({server_count: bot.guilds.size})
+    .end(err => {
+      if (err) return console.error(err);
+      console.log("Success (pwbot)!");
     });
 
   bot.user.setPresence({status: 'online', game: {name: '!go fish | !help'}});
