dc25185867da75de1ecc13c808f4ebb6cc41ce41
Brenda Zhang <brenda.zhang2@gmail.com>
2017-08-05 16:29:11 -0700

    Add users to info, add null member check

--- src/commands/info.js
+++ src/commands/info.js
@@ -2,7 +2,7 @@ const getInfo = (bot, msg, cmd, richEmbed) => {
   if (cmd === 'info') {
     msg.channel.send({
       embed: richEmbed.setColor('#ff0000').setDescription(
-        'This bot is running on ' + bot.guilds.size + ' servers and ' + bot.channels.array().length + ' channels.'
+        'This bot is running on ' + bot.guilds.size + ' servers and ' + bot.channels.size + ' channels with ' + bot.users.size + '.'
       )
     });
   }
