84f4dd1c2e829ad0e5ecaba576695dae23160973
Brenda Zhang <brenda.zhang2@gmail.com>
2017-08-06 16:27:20 -0700

    Add links to server in info and help

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -8,7 +8,8 @@ const helpCmd = (msg, cmd, richEmbed) => {
 ${helpList.headings.stop} ${helpList.commands.stop}
 ${helpList.headings.delete} ${helpList.commands.delete}
 ${helpList.headings.go} ${helpList.commands.go}
-For more information, visit the [Stop Bot GitHub](https://github.com/brendacs/stop-bot)`)
+For more information, visit the [Stop Bot GitHub](https://github.com/brendacs/stop-bot).
+To give feedback and contribute, join the [Stop Discord Server](https://discord.gg/HwkMkKh).`)
     
   if (cmd === 'help') {
     msg.channel.send({embed});
