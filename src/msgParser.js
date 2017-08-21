fa6ebca890ce4be95ab3da97ea517027843dff2a
Brenda Zhang <brenda.zhang2@gmail.com>
2017-08-21 14:20:54 -0700

    Check messages beginning with exclamation marks, prevent sending of multiple stop messages for one message

--- src/msgParser.js
+++ src/msgParser.js
@@ -2,13 +2,33 @@ import commands from './commands/commands.js';
 
 const msgParser = (bot, stopClient, msg, admin, mod, thisGuild, stopList, deleteList) => {
   const string = msg.content;
+  const cmds = ['help', 'info', 'stop', 'delete', 'go'];
+
+  const checkMessage = () => {
+    for (let i = 0; i < stopList.length; i++) {
+      if (string.toString().toLowerCase().indexOf(stopList[i]) !== -1) {
+        msg.channel.send(new Date().toString());
+        msg.reply('IT\'S TIME TO STOP.');
+        return; // prevents from sending multiple stop messages if multiple stopped words detected in one msg
+      }
+    }
+    for (let i = 0; i < deleteList.length; i++) {
+      if (string.toString().toLowerCase().indexOf(deleteList[i]) !== -1) {
+        msg.delete(1000);
+        msg.channel.send('Detected something horrible. Deleted.');
+      }
+    }
+  }
 
   if (msg.toString().substring(0, 1) === '!') { // if prefix is used
     const args = msg.toString().substring(1).split(' ');
     const cmd = args[0];
     const subcmd = args[1];
-    if (stopList && deleteList) {
+    if (stopList && deleteList && cmds.indexOf(cmd) !== -1) {
       commands(bot, stopClient, msg, cmd, subcmd, admin, mod, thisGuild, stopList, deleteList);
+    } else {
+      // check messages beginning with exclamation marks and are not commands
+      checkMessage();
     }
   } else if (msg.mentions.users.has('340404757648769025')) { // if bot is mentioned
     const args = msg.toString().split(' ');
@@ -18,18 +38,7 @@ const msgParser = (bot, stopClient, msg, admin, mod, thisGuild, stopList, delete
       commands(bot, stopClient, msg, cmd, subcmd, admin, mod, thisGuild, stopList, deleteList);
     }
   } else {
-    for (let i = 0; i < stopList.length; i++) {
-      if (string.toString().toLowerCase().indexOf(stopList[i]) !== -1) {
-        msg.channel.send(new Date().toString());
-        msg.reply('IT\'S TIME TO STOP.');
-      }
-    }
-    for (let i = 0; i < deleteList.length; i++) {
-      if (string.toString().toLowerCase().indexOf(deleteList[i]) !== -1) {
-        msg.delete(1000);
-        msg.channel.send('Detected something horrible. Deleted.');
-      }
-    }
+    checkMessage();
   }
 }
 
