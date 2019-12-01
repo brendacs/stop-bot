728300e7cde95a44049503c65e3188162288a2d4
brendacs <brenda.zhang2@gmail.com>
2019-11-30 22:28:40 -0800

    Modularize stop and delete code and rearrange for readability

--- src/commands/delete.js
+++ src/commands/delete.js
@@ -4,77 +4,80 @@ import { isAdmin, isMod } from '../utils/checkPerms';
 import { isOnStopList, isOnDeleteList } from '../utils/checkLists';
 import { getGuildId } from '../utils/utils';
 
-const deleteCmd = (msg, subcmd, stopList, deleteList) => {
+export default function deleteCmd(msg, subcmd, stopList, deleteList) {
   if (!subcmd) return;
-
-  if (subcmd === 'list') {
-    if (deleteList.length !== 0) {
-      getToggleDM(msg).then(enabled => {
-        if (enabled) {
-          msg.member.createDM()
-            .then(channel => {
-              channel.send('These words are currently being deleted: `' + deleteList.join(', ') + '`');
-            });
-        } else {
-          msg.channel.send('These words are currently being deleted: `' + deleteList.join(', ') + '`');
-        }
-      });
-    } else {
-      msg.channel.send('There are no words on the delete list.')
-    }
-  } else if (!isNaN(subcmd) && isMod(msg)) {
-    let limit = parseInt(subcmd) + 1;
-    if (limit >= 100) {
-      msg.channel.send({
-        embed: richEmbed.setColor('#ff0000').setDescription(`Number of messages must be under 100.`)
-      });
-      return;
-    }
-    msg.channel.fetchMessages({limit: limit})
-      .then((latestMessages) => {
-        let mentions = msg.mentions.users;
-        let mentionsNum = msg.mentions.users.array().length;
-        if (mentionsNum === 0) msg.channel.bulkDelete(latestMessages);
-        else {
-          let latestByUser = latestMessages.filter((message) => message.author === mentions.first());
-          msg.channel.bulkDelete(latestByUser);
-        }
-      })
-      .catch(err => console.log(err));
-  } else if (subcmd && isNaN(subcmd)) {
-    if (isAdmin(msg)) {
-
-      // Check if any reserved words are set to be deleted
-      let wordsToBeDeleted = subcmd.split(', ');
-      for (let word of wordsToBeDeleted) {
-        if (reservedWords.indexOf(word) !== -1) {
-          msg.channel.send({
-            embed: richEmbed.setColor('#ff0000').setDescription(`This word is reserved for bot functionality and cannot be deleted.`)
-          });
-          return;
-        }
-      }
-
-      const isStopped = isOnStopList(stopList, subcmd);
-      const isDeleted = isOnDeleteList(deleteList, subcmd);
-
-      if (isDeleted) {
-        msg.channel.send('`' + subcmd + '`' + ' is already on the list of words to delete.');
-      } else if (isStopped) {
-        msg.channel.send('`' + subcmd + '`' + ' is already on the list of words to stop.');
-      } else {
-        const guildId = getGuildId(msg);
-        subcmd = subcmd.replace(/'/g, "''");
-        stopClient.query(`UPDATE word_lists SET deletelist = deletelist || '{${subcmd.toLowerCase()}}' WHERE serverid=${guildId}`);
-        subcmd = subcmd.replace(/''/g, "'");
-        msg.channel.send('`' + subcmd + '`' + ' will be deleted every time it appears.');
-      }
-    } else if (!isAdmin(msg)) {
-      msg.reply('you can\'t use this command.');
-    }
-  } else {
-    return;
-  }
+  if (subcmd === 'list') getList(msg, deleteList);
+  else if (!isNaN(subcmd) && isMod(msg)) bulkDelete(msg, subcmd);
+  else if (subcmd && isNaN(subcmd)) addToList(msg, subcmd, stopList, deleteList);
+  return;
 }
 
-export default deleteCmd;
+function addToList(msg, subcmd, stopList, deleteList) {
+  if (isAdmin(msg)) {
+    // Check if any reserved words are set to be deleted
+    let wordsToBeDeleted = subcmd.split(', ');
+    for (let word of wordsToBeDeleted) {
+      if (reservedWords.indexOf(word) !== -1) {
+        msg.channel.send({
+          embed: richEmbed.setColor('#ff0000').setDescription(`This word is reserved for bot functionality and cannot be deleted.`)
+        });
+        return;
+      }
+    }
+
+    const isStopped = isOnStopList(stopList, subcmd);
+    const isDeleted = isOnDeleteList(deleteList, subcmd);
+
+    if (isDeleted || isStopped) {
+      msg.channel.send(`\`${subcmd}\` is already on the list of words to ${isDeleted ? 'delete' : 'stop'}.`);
+      return;
+    }
+
+    const guildId = getGuildId(msg);
+    subcmd = subcmd.replace(/'/g, "''");
+    stopClient.query(`UPDATE word_lists SET deletelist = deletelist || '{${subcmd.toLowerCase()}}' WHERE serverid=${guildId}`);
+    subcmd = subcmd.replace(/''/g, "'");
+    msg.channel.send(`\`${subcmd}\` will be deleted every time it appears.`);
+    return;
+  }
+
+  msg.reply('you must have the proper permissions use this command.');
+}
+
+function bulkDelete(msg, subcmd) {
+  let limit = parseInt(subcmd) + 1;
+  if (limit >= 100) {
+    msg.channel.send({
+      embed: richEmbed.setColor('#ff0000').setDescription(`Number of messages must be under 100.`)
+    });
+    return;
+  }
+  msg.channel.fetchMessages({ limit: limit })
+    .then((latestMessages) => {
+      let mentions = msg.mentions.users;
+      let mentionsNum = msg.mentions.users.array().length;
+      if (mentionsNum === 0) msg.channel.bulkDelete(latestMessages);
+      else {
+        let latestByUser = latestMessages.filter((message) => message.author === mentions.first());
+        msg.channel.bulkDelete(latestByUser);
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+function getList(msg, deleteList) {
+  if (deleteList.length !== 0) {
+    getToggleDM(msg).then(enabled => {
+      if (enabled) {
+        msg.member.createDM()
+          .then(channel => {
+            channel.send(`These words are currently being deleted: \`${deleteList.join(', ')}\``);
+          });
+      } else {
+        msg.channel.send(`These words are currently being deleted: \`${deleteList.join(', ')}\``);
+      }
+    });
+  } else {
+    msg.channel.send('There are no words on the delete list.');
+  }
+}
--- src/commands/stop.js
+++ src/commands/stop.js
@@ -4,66 +4,68 @@ import { isAdmin } from '../utils/checkPerms';
 import { isOnStopList, isOnDeleteList } from '../utils/checkLists';
 import { getGuildId } from '../utils/utils';
 
-const stopCmd = (msg, subcmd, stopList, deleteList) => {
+export default function stopCmd(msg, subcmd, stopList, deleteList) {
   let mentions = msg.mentions.users;
-  let mentionsNum = msg.mentions.users.array().length;
-
-  getStopMessage(msg).then(stopMessage => {
-    if (!subcmd) {
-      msg.channel.send(stopMessage);
-    } else if (mentions.first() !== undefined) {
-      msg.channel.send(`${mentions.first(mentionsNum)}\n${stopMessage}`);
-    } else if (subcmd === 'video') {
-      msg.channel.send('https://www.youtube.com/watch?v=2k0SmqbBIpQ');
-    } else if (subcmd === 'list') {
-      if (stopList.length !== 0) {
-        getToggleDM(msg).then(enabled => {
-          if (enabled) {
-            msg.member.createDM()
-              .then(channel => {
-                channel.send('These words are currently being stopped: `' + stopList.join(', ') + '`');
-              });
-          } else {
-            msg.channel.send('These words are currently being stopped: `' + stopList.join(', ') + '`');
-          }
-        });
-      } else {
-        msg.channel.send('There are no words on the stop list.');
-      }
-    } else if (subcmd) {
-      if (isAdmin(msg)) {
-
-        // Check if any reserved words are set to be deleted
-        let wordsToBeStopped = subcmd.split(', ');
-        for (let word of wordsToBeStopped) {
-          if (subcmd !== 'video' && reservedWords.indexOf(word) !== -1) {
-            msg.channel.send({
-              embed: richEmbed.setColor('#ff0000').setDescription(`This word is reserved for bot functionality and cannot be stopped.`)
-            });
-            return;
-          }
-        }
-
-        const guildId = getGuildId(msg);
-        
-        const isStopped = isOnStopList(stopList, subcmd);
-        const isDeleted = isOnDeleteList(deleteList, subcmd);
-
-        if (isStopped) {
-          msg.channel.send('`' + subcmd + '`' + ' is already on the list of words to stop.');
-        } else if (isDeleted) {
-          msg.channel.send('`' + subcmd + '`' + ' is already on the list of words to delete.');
-        } else {
-          subcmd = subcmd.replace(/'/g, "''");
-          stopClient.query(`UPDATE word_lists SET stoplist = stoplist || '{${subcmd.toLowerCase()}}' WHERE serverid=${guildId}`);
-          subcmd = subcmd.replace(/''/g, "'");
-          msg.channel.send('`' + subcmd + '`' + ' will now be stopped.');
-        }
-      } else if (!isAdmin(msg)) {
-        msg.reply('you can\'t use this command.');
-      }
-    } else return;
-  });
+  if (!subcmd || mentions.first()) sendStopMessage(msg, subcmd, mentions);
+  else if (subcmd === 'video') msg.channel.send('https://www.youtube.com/watch?v=2k0SmqbBIpQ');
+  else if (subcmd === 'list') getList(msg, stopList);
+  else if (subcmd) addToList(msg, subcmd, stopList, deleteList);
 }
 
-export default stopCmd;
+function addToList(msg, subcmd, stopList, deleteList) {
+  if (isAdmin(msg)) {
+    // Check if any reserved words are set to be stopped
+    let wordsToBeStopped = subcmd.split(', ');
+    for (let word of wordsToBeStopped) {
+      if (subcmd !== 'video' && reservedWords.indexOf(word) !== -1) {
+        msg.channel.send({
+          embed: richEmbed.setColor('#ff0000').setDescription(`This word is reserved for bot functionality and cannot be stopped.`)
+        });
+        return;
+      }
+    }
+
+    const isStopped = isOnStopList(stopList, subcmd);
+    const isDeleted = isOnDeleteList(deleteList, subcmd);
+
+    if (isDeleted || isStopped) {
+      msg.channel.send(`\`${subcmd}\` is already on the list of words to ${isDeleted ? 'delete' : 'stop'}.`);
+      return;
+    }
+
+    const guildId = getGuildId(msg);
+    subcmd = subcmd.replace(/'/g, "''");
+    stopClient.query(`UPDATE word_lists SET stoplist = stoplist || '{${subcmd.toLowerCase()}}' WHERE serverid=${guildId}`);
+    subcmd = subcmd.replace(/''/g, "'");
+    msg.channel.send(`\`${subcmd}\` will now be stopped.`);
+    return;
+  }
+
+  msg.reply('you must have the proper permissions use this command.');
+}
+
+function getList(msg, stopList) {
+  if (stopList.length !== 0) {
+    getToggleDM(msg).then(enabled => {
+      if (enabled) {
+        msg.member.createDM()
+          .then(channel => {
+            channel.send(`These words are currently being stopped: \`${stopList.join(', ')}\``);
+          });
+      } else {
+        msg.channel.send(`These words are currently being stopped: \`${stopList.join(', ')}\``);
+      }
+    });
+  } else {
+    msg.channel.send('There are no words on the stop list.');
+  }
+}
+
+function sendStopMessage(msg, subcmd, mentions) {
+  let mentionsNum = mentions.array().length;
+  getStopMessage(msg).then(stopMessage => {
+    if (!subcmd) msg.channel.send(stopMessage);
+    else if (mentions.first()) msg.channel.send(`${mentions.first(mentionsNum)}\n${stopMessage}`);
+    return;
+  });
+}
